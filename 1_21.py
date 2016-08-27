46fd7aba7030225af488cb9de4b8c2e71e18041e
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-08-27 18:15:06 -0400

    Adding plot of n vs heads frequency

--- 1_21.py
+++ 1_21.py
@@ -1,9 +1,11 @@
 import random
+import matplotlib.pyplot as plt
 
 p=0.3
 headsfreq=[]
+maxtrials=10000
 
-for n in range(10000):
+for n in range(maxtrials):
 	heads=0.0
 	for i in range(n+1):
 		if(random.random()<=p):
@@ -11,4 +13,6 @@ for n in range(10000):
 
 	headsfreq.append(heads/(n+1))
 
-print(headsfreq)
+#print(headsfreq)
+plt.plot(range(maxtrials),headsfreq)
+plt.show()
