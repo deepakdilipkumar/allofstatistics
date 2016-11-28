1261440794cf8377b12a067e892069a14c19e08e
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-11-28 14:54:03 -0500

    Adding bootstrap estimation

---
+++ HW9/bootstrap.py
@@ -0,0 +1,31 @@
+import numpy.random as rnd
+import numpy as np
+
+B=10000
+N1=50
+N2=50
+
+p1mle=0.3
+p2mle=0.4
+
+estimate=[]
+
+for i in range(B):
+
+	p1=0.0
+	for j in range(N1):
+		if(rnd.uniform(0,1)<p1mle):
+			p1+=1
+
+	p1/=N1
+
+	p2=0.0
+	for j in range(N2):
+		if(rnd.uniform(0,1)<p2mle):
+			p2+=1
+
+	p2/=N2
+
+	estimate.append(p2-p1)
+
+print(np.std(estimate))
